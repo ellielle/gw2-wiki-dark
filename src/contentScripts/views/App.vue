096c458d501fbf64ff9d0d6eecc87f317fe4316a
ellielle <40385743+ellielle@users.noreply.github.com>
2023-08-15 11:35:03 -0400

    refactor: use a string for messaging and storage

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,28 +1,16 @@
-<script setup lang="ts">
-import { useToggle } from '@vueuse/core'
-import 'uno.css'
-
-const [show, toggle] = useToggle(false)
-</script>
-
 <template>
   <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
     <div
-      class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
+      class="bg-white text-gray-800 rounded-lg shadow w-max h-min opacity-0"
       p="x-4 y-2"
       m="y-auto r-2"
       transition="opacity duration-300"
-      :class="show ? 'opacity-100' : 'opacity-0'"
     >
-      <h1 class="text-lg">
-        Vitesse WebExt mysterious app.vue
-      </h1>
-      <SharedSubtitle />
+      <h1 class="text-lg">This must exist or everything will die</h1>
     </div>
     <button
       class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none"
       bg="teal-600 hover:teal-700"
-      @click="toggle()"
     >
       <pixelarticons-power class="block m-auto text-white text-lg" />
     </button>
--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,5 @@
+import type { RemovableRef } from "@vueuse/core";
 import { useStorageLocal } from "~/composables/useStorageLocal";
 
-export const isDark = useStorageLocal("is-dark", false);
+export type ColorModeType = "dark" | "light";
+export const colorMode: RemovableRef<ColorModeType> = useStorageLocal("color-mode", "dark");
