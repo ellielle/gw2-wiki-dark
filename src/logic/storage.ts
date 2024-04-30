be2a982e0f716a25ac12df7ffbedf0e6339258c5
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-30 09:02:31 -0400

    remove more TS build bloat

--- src/logic/storage.ts
+++
@@ -1,5 +0,0 @@
-import type { RemovableRef } from "@vueuse/core";
-import { useStorageLocal } from "~/composables/useStorageLocal";
-
-export type ColorModeType = "dark" | "light";
-export const colorMode: RemovableRef<ColorModeType> = useStorageLocal("color-mode", "dark");
